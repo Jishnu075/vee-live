e31db0206cc731f341916b4d84f1f8dceb5b5a08
Jishnu Biju <43580135+Jishnu075@users.noreply.github.com>
2021-07-17 15:51:43 +0530

    Update settings.py

--- vee/settings.py
+++ vee/settings.py
@@ -163,3 +163,21 @@ LOGIN_URL = 'web_login'
 
 # Activate Django-Heroku.
 # django_heroku.settings(locals())
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
